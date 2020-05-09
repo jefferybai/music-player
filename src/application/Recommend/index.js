92779ec4a565045e767cd70a73e503538fb79f6f
jeffery.bai <372071865@qq.com>
2020-05-09 15:32:28 +0800

    add redux

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,81 +1,55 @@
-import React from 'react';
-import Slider from '../../components/slider/index';
-import RecommendList from '../../components/recommendlist';
-import Scroll from '../../components/scoll';
-import { Content } from '../../components/content';
+import React, { useEffect } from 'react';
+import Slider from '../../components/slider/';
+import { connect } from "react-redux";
+import * as actionTypes from './store/actionCreators';
+import RecommendList from '../../components/list/';
+import Scroll from '../../baseUI/scroll/index';
+import { Content } from './style';
 
-// import { PureComponent, Component } from 'react';
+function Recommend (props){
+  const { bannerList, recommendList } = props;
 
-// class Rr extends Component {
+  const { getBannerDataDispatch, getRecommendListDataDispatch } = props;
 
-//     state = {
-//         parentMsg: 'parent',
-//         sonMsg: {
-//             val: 'this is val of son'
-//         }
-//     }
-//     render() {
-//         console.log(123213123);
-//         this.state.parentMsg = 'asdfsadfsf' + Date.now();
-//         return (
-//             <div className="App">
-//                 <header className="App-header" onClick={() => this.setState(this.state)}>
-//                     <p>
-//                         {this.state.parentMsg}
-//                     </p>
-//                 </header>
-//                 <button onClick={() =>
-//                     this.setState(({ sonMsg }) => {
-//                         sonMsg.val = 'son' + Date.now();
-//                         console.table(sonMsg);
-//                         return { sonMsg }
-//                     })
-//                 }>修改子组件props</button>
-//                 <ComponentSon sonMsg={this.state.sonMsg} />
-//             </div>
-//         );
-//     }
-// }
+  useEffect (() => {
+    getBannerDataDispatch ();
+    getRecommendListDataDispatch ();
+    //eslint-disable-next-line
+  }, []);
 
-// export class ComponentSon extends PureComponent {
-//     render() {
-//         console.log('PureComponent rendered : ' + Date.now());
-//         const msg = this.props.sonMsg;
-//         return (
-//             <div> {msg.val}</div>
-//         )
-//     }
-// }
+  const bannerListJS = bannerList ? bannerList.toJS () : [];
+  const recommendListJS = recommendList ? recommendList.toJS () :[];
 
-function Recommend(props) {
-
-
-    //mock 数据
-    const bannerList = [1, 2, 3, 4].map(item => {
-        return { imageUrl: "http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg" }
-    });
-
-    const recommendList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(item => {
-        return {
-            id: 1,
-            picUrl: "https://p1.music.126.net/fhmefjUfMD-8qtj3JKeHbA==/18999560928537533.jpg",
-            playCount: 17171122,
-            name: "朴树、许巍、李健、郑钧、老狼、赵雷"
-        }
-    });
-    return (
-        <Content>
-            <Scroll className="list">
-                <div>
-                    <Slider bannerList={bannerList}></Slider>
-                    <RecommendList recommendList={recommendList}></RecommendList>
-                </div>
-            </Scroll>
-        </Content>
-    )
-
+  return (
+    <Content>
+      <Scroll>
+        <div>
+          <Slider bannerList={bannerListJS}></Slider>
+          <RecommendList recommendList={recommendListJS}></RecommendList>
+        </div>
+      </Scroll>
+    </Content> 
+  );
 }
 
-export default React.memo(Recommend)
+// 映射 Redux 全局的 state 到组件的 props 上
+const mapStateToProps = (state) => ({
+  // 不要在这里将数据 toJS
+  // 不然每次 diff 比对 props 的时候都是不一样的引用，还是导致不必要的重渲染，属于滥用 immutable
+  bannerList: state.getIn (['recommend', 'bannerList']),
+  recommendList: state.getIn (['recommend', 'recommendList']),
+});
+// 映射 dispatch 到 props 上
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getBannerDataDispatch () {
+      dispatch (actionTypes.getBannerList ());
+    },
+    getRecommendListDataDispatch () {
+      dispatch (actionTypes.getRecommendList ());
+    },
+  }
+};
 
-
+// 将 ui 组件包装成容器组件
+export default connect (mapStateToProps, mapDispatchToProps)(React.memo (Recommend));
